42e2f2b61119eb2add36f330edaf585294a31a51
lwticias-lelet <leticiadaraujo1@gmail.com>
2026-06-07 12:03:50 -0300

    implementa geração de código Assembly para operações VM
    - Adiciona suporte a comandos aritméticos e lógicos
    - Implementa comparações (eq, gt, lt) com labels únicos
    - Adiciona suporte a push/pop dos segmentos:
      local, argument, this, that, temp, pointer, static e constant
    - Implementa gerenciamento da pilha (SP)
    - Adiciona abstração para operações unárias e binárias
    - Inclui geração de símbolos estáticos por classe

---
+++ codewriter/codewriter.py
@@ -0,0 +1,193 @@
+import os
+
+
+class CodeWriter:
+
+    def __init__(self, filename):
+
+        self.file = open(filename, "w")
+        self.label_counter = 0
+
+        self.class_name = os.path.basename(filename).replace(".asm", "")
+
+    def write_line(self, text):
+        self.file.write(text + "\n")
+
+    def push_d(self):
+
+        self.write_line("@SP")
+        self.write_line("A=M")
+        self.write_line("M=D")
+
+        self.write_line("@SP")
+        self.write_line("M=M+1")
+
+    def write_arithmetic(self, command):
+
+        if command == "add":
+            self.binary("M=M+D")
+
+        elif command == "sub":
+            self.binary("M=M-D")
+
+        elif command == "and":
+            self.binary("M=M&D")
+
+        elif command == "or":
+            self.binary("M=M|D")
+
+        elif command == "neg":
+            self.unary("M=-M")
+
+        elif command == "not":
+            self.unary("M=!M")
+
+        elif command == "eq":
+            self.compare("JEQ")
+
+        elif command == "gt":
+            self.compare("JGT")
+
+        elif command == "lt":
+            self.compare("JLT")
+
+    def unary(self, operation):
+
+        self.write_line("@SP")
+        self.write_line("A=M-1")
+        self.write_line(operation)
+
+    def binary(self, operation):
+
+        self.write_line("@SP")
+        self.write_line("AM=M-1")
+        self.write_line("D=M")
+
+        self.write_line("A=A-1")
+        self.write_line(operation)
+
+    def compare(self, jump):
+
+        true_label = f"TRUE_{self.label_counter}"
+        end_label = f"END_{self.label_counter}"
+
+        self.label_counter += 1
+
+        self.write_line("@SP")
+        self.write_line("AM=M-1")
+        self.write_line("D=M")
+
+        self.write_line("A=A-1")
+        self.write_line("D=M-D")
+
+        self.write_line(f"@{true_label}")
+        self.write_line(f"D;{jump}")
+
+        self.write_line("@SP")
+        self.write_line("A=M-1")
+        self.write_line("M=0")
+
+        self.write_line(f"@{end_label}")
+        self.write_line("0;JMP")
+
+        self.write_line(f"({true_label})")
+
+        self.write_line("@SP")
+        self.write_line("A=M-1")
+        self.write_line("M=-1")
+
+        self.write_line(f"({end_label})")
+
+    def write_push(self, segment, index):
+
+        if segment == "constant":
+
+            self.write_line(f"@{index}")
+            self.write_line("D=A")
+
+        elif segment in ["local", "argument", "this", "that"]:
+
+            base = {
+                "local": "LCL",
+                "argument": "ARG",
+                "this": "THIS",
+                "that": "THAT"
+            }[segment]
+
+            self.write_line(f"@{base}")
+            self.write_line("D=M")
+
+            self.write_line(f"@{index}")
+            self.write_line("A=D+A")
+            self.write_line("D=M")
+
+        elif segment == "temp":
+
+            self.write_line(f"@{5+index}")
+            self.write_line("D=M")
+
+        elif segment == "pointer":
+
+            if index == 0:
+                self.write_line("@THIS")
+            else:
+                self.write_line("@THAT")
+
+            self.write_line("D=M")
+
+        elif segment == "static":
+
+            self.write_line(f"@{self.class_name}.{index}")
+            self.write_line("D=M")
+
+        self.push_d()
+
+    def write_pop(self, segment, index):
+
+        if segment in ["local", "argument", "this", "that"]:
+
+            base = {
+                "local": "LCL",
+                "argument": "ARG",
+                "this": "THIS",
+                "that": "THAT"
+            }[segment]
+
+            self.write_line(f"@{base}")
+            self.write_line("D=M")
+
+            self.write_line(f"@{index}")
+            self.write_line("D=D+A")
+
+        elif segment == "temp":
+
+            self.write_line(f"@{5+index}")
+            self.write_line("D=A")
+
+        elif segment == "pointer":
+
+            if index == 0:
+                self.write_line("@THIS")
+            else:
+                self.write_line("@THAT")
+
+            self.write_line("D=A")
+
+        elif segment == "static":
+
+            self.write_line(f"@{self.class_name}.{index}")
+            self.write_line("D=A")
+
+        self.write_line("@R13")
+        self.write_line("M=D")
+
+        self.write_line("@SP")
+        self.write_line("AM=M-1")
+        self.write_line("D=M")
+
+        self.write_line("@R13")
+        self.write_line("A=M")
+        self.write_line("M=D")
+
+    def close(self):
+        self.file.close()
