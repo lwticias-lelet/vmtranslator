38742e739a363c2c7f230fcd734c76c423c4623b
lwticias-lelet <leticiadaraujo1@gmail.com>
2026-06-07 12:00:54 -0300

    Adiciona Parser com leitura de arquivo e identificação de comandos

---
+++ parser/paser.py
@@ -0,0 +1,51 @@
+class Parser:
+
+    def __init__(self, filename):
+
+        self.commands = []
+
+        with open(filename, "r") as file:
+
+            for line in file:
+
+                line = line.split("//")[0].strip()
+
+                if line:
+                    self.commands.append(line)
+
+        self.index = 0
+        self.current_command = None
+
+    def has_more_commands(self):
+        return self.index < len(self.commands)
+
+    def advance(self):
+
+        self.current_command = self.commands[self.index]
+        self.index += 1
+
+    def command_type(self):
+
+        cmd = self.current_command.split()[0]
+
+        if cmd == "push":
+            return "C_PUSH"
+
+        if cmd == "pop":
+            return "C_POP"
+
+        return "C_ARITHMETIC"
+
+    def arg1(self):
+
+        if self.command_type() == "C_ARITHMETIC":
+            return self.current_command.split()[0]
+
+        return self.current_command.split()[1]
+
+    def arg2(self):
+
+        if self.command_type() in ["C_PUSH", "C_POP"]:
+            return int(self.current_command.split()[2])
+
+        return None
