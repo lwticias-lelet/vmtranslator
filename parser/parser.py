f5c85f8f5c0646d74ab900ec166e7227c3f84c21
lwticias-lelet <leticiadaraujo1@gmail.com>
2026-06-24 19:32:39 -0300

    adicionar classificação de tipo de comando VMppelo metodo command_type

--- parser/parser.py
+++ parser/parser.py
@@ -23,3 +23,32 @@ class Parser:
 
         self.current_command = self.commands[self.index]
         self.index += 1
+            def command_type(self):
+
+        cmd = self.current_command.split()[0]
+
+        if cmd == "push":
+            return "C_PUSH"
+
+        if cmd == "pop":
+            return "C_POP"
+
+        if cmd == "label":
+            return "C_LABEL"
+
+        if cmd == "goto":
+            return "C_GOTO"
+
+        if cmd == "if-goto":
+            return "C_IF"
+
+        if cmd == "function":
+            return "C_FUNCTION"
+
+        if cmd == "call":
+            return "C_CALL"
+
+        if cmd == "return":
+            return "C_RETURN"
+
+        return "C_ARITHMETIC"
